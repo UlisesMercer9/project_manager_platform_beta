27710ee4ef51ab8eb0b65f14b6010d97c845d1b4
Ulises Rodríguez <ulisesmercer9@gmail.com>
2021-04-12 18:47:00 -0500

    feat: Consulting the API

---
+++ app/GraphQL/Queries/ProjectsQuery.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace App\GraphQL\Queries;
+use App\Models\Project;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Query;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+
+class ProjectsQuery extends Query
+{
+	protected $attributes = [
+		'name' => 'The Project Query',
+		'description' => 'Retrieves project'
+	];
+
+	public function type(): Type
+	{
+		return Type::listOf(GraphQL::type('project'));
+	}
+
+	public function resolve($root, $args)
+	{
+		return Project::all();
+	}
+}
