a5c881c728da1c703902d541a48fcd7839623a8c
Ulises Rodríguez <ulisesmercer9@gmail.com>
2021-04-12 18:28:44 -0500

    feat: Understanding GraphQL

---
+++ app/GraphQL/Types/ProjectType.php
@@ -0,0 +1,42 @@
+<?php  
+
+namespace App\GraphQL\Types;
+
+use App\Models\Project;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Type as GraphQLType;
+
+class ProjectType extends GraphQLType
+{
+	protected $attributes = [
+		'name' => 'Project',
+		'description' => 'A Project Type',
+		'model' => Project::class
+	];
+
+	public function fields(): array
+	{
+		return [
+			'id' => [
+				'type' => Type::nonNull(Type::int()),
+				'description' => 'The Project Id'
+			],
+			'title' => [
+				'type' => Type::nonNull(Type::string()),
+				'description' => 'The Project title'
+			],
+			'description' => [
+				'type' => Type::nonNull(Type::string()),
+				'description' => 'The Project description'
+			],
+		// 	'manager' => [
+		// 		'type' => Type::nonNull(Type::int()),
+		// 		'description' => 'The Project'
+		// 	],
+		// 	'tasks' => [
+		// 		'type' => Type::nonNull(Type::int()),
+		// 		'description' => 'The Project'
+		// 	],
+		];
+	}
+}
